b7e2f9b54b90b7d59d7a596a69d8a437ecacd2df
mingyou <mingyou@gateweb.com>
2024-07-17 21:36:26 +0800

    commit test 3

--- src/main/java/com/gnim/maven/Main.java
+++ src/main/java/com/gnim/maven/Main.java
@@ -2,6 +2,6 @@ package com.gnim.maven;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world2!");
     }
 }
